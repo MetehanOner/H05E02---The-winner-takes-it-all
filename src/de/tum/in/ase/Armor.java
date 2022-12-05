4983b1f591bb35a5fd91b32bd7a61ab0bf7325d6
MetehanOner <a.metehanoner@gmail.com>
2022-12-05 15:03:35 +0100

    init for part2

--- src/de/tum/in/ase/Armor.java
+++ src/de/tum/in/ase/Armor.java
@@ -1,5 +1,22 @@
 package de.tum.in.ase;
 
-public class Armor{
+public class Armor extends Item {
   // TODO: Implement part 2.
+    private int amountOfArmor;
+
+    public Armor(String armorType, String specification, int amountOfArmor, int strength, int intelligence, int agility, int spirit){
+        super(armorType);
+        this.amountOfArmor = amountOfArmor;
+
+        super.specification = specification;
+        super.strength = strength;
+        super.intelligence = intelligence;
+        super.agility = agility;
+        super.spirit = spirit;
+
+    }
+
+    public int getAmountOfArmor() {
+        return amountOfArmor;
+    }
 }
--- src/de/tum/in/ase/Weapon.java
+++ src/de/tum/in/ase/Weapon.java
@@ -1,5 +1,24 @@
 package de.tum.in.ase;
 
-public class Weapon {
+public class Weapon extends Item {
   // TODO: Implement part 2.
+    private int damage;
+
+    public Weapon(String weaponType, String specification, int damage, int strength, int intelligence, int agility, int spirit) {
+        super(weaponType);
+
+        super.specification = specification;
+
+        this.damage = damage;
+
+        super.strength = strength;
+        super.intelligence = intelligence;
+        super.agility = agility;
+        super.spirit = spirit;
+
+    }
+
+    public int getDamage() {
+        return damage;
+    }
 }
